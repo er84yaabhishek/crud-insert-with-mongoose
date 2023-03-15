8718004229b11ae0c4fcd10fda6733a02010c13b
er84yaabhishek <er84yaabhishek@gmail.com>
2023-03-15 15:42:39 +0530

    insert data with mongoose

---
+++ index.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const main = async () => {
+  await mongoose.connect("mongodb://127.0.0.1:27017/e-com");
+  const ProductSchema = new mongoose.Schema({
+    name: String,
+  });
+  const ProductsModel = mongoose.model("products", ProductSchema);
+  let data = new ProductsModel({ name: "m8" });
+  let result = await data.save();
+  console.log(result);
+};
+main();
